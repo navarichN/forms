61aa40bd15bc08478c3493964916aa6ed7c73489
navarichN <54509830+navarichN@users.noreply.github.com>
2021-03-24 22:02:45 +0100

    feat: add conditional rendering of data output

--- hometasks/src/components/Steps/Steps.js
+++ hometasks/src/components/Steps/Steps.js
@@ -4,24 +4,49 @@ import {PencilIcon, XCircleIcon} from '@primer/octicons-react';
 export default function Steps(){
     const [data, setData] = useState({});
     const [date, setDate] = useState('');
-    const [dist, setDist] = useState('');
+    const [dist, setDist] = useState(0);
 
     const onSubmit = (e) => {
         // setData(data[date] = dist)
         if(data.hasOwnProperty(date)) {
-            console.log('date=',date, 'data[date]=', data[date])
-            console.log('data:',data)
-            data[date] = data[date]  + dist        
+           console.log(data[date])
+           setData(data[date]+=dist)
         }
+
         setData(prev => ({
             ...prev,
             [date] : dist
         }))
+
             
         console.log(data)
         e.preventDefault();
     }
-    
+    let table = (
+        <table className="output">
+        <thead>
+            <tr className="output-head">
+                <th>Дата(ДД.ММ.ГГ)</th>
+                <th>Пройдено км</th>
+                <th>Действия</th>
+            </tr>
+        </thead>
+        <tbody className="output-body">
+            {Object.entries(data).map((el) => {
+                return (
+                    <tr className="output-item">
+                        <td>{el[0]}</td>
+                        <td>{el[1]}</td>
+                        <td className="output-item-buttons">
+                            <button className="pen"><PencilIcon size={15}/></button>
+                            <button className="cross"><XCircleIcon size={16}/></button>
+                        </td>
+                    </tr>
+                )
+            })}
+        </tbody>
+    </table>
+    )
 
     return(
         <div className="steps">
@@ -33,36 +58,16 @@ export default function Steps(){
                 
                 <label htmlFor="distance">
                     Пройдено км
-                    <input type="text" name="distance" id="distance" onChange={(e) => setDist(e.target.value)}></input>
+                    <input type="text" name="distance" id="distance" onChange={(e) => setDist(Number(e.target.value))}></input>
                 </label>
                 
-                <label htmlFor="adding">
+                <label className="adding" htmlFor="adding">
+                    ''
                     <input type="submit" name="adding" id="adding" value="Ok" onClick={onSubmit}></input>
                 </label>
             </form>
             <div>
-                <table className="output">
-                    <tr className="output-head">
-                        <th>Дата(ДД.ММ.ГГ)</th>
-                        <th>Пройдено км</th>
-                        <th>Действия</th>
-                    </tr>
-                    <tbody className="output-body">
-                        {Object.entries(data).map((el) => {
-                            return (
-                                <tr className="output-item">
-                                    <td>{el[0]}</td>
-                                    <td>{el[1]}</td>
-                                    <td className="output-item-buttons">
-                                        <button className="pen"><PencilIcon size={15}/></button>
-                                        <button className="cross"><XCircleIcon size={16}/></button>
-                                    </td>
-                                </tr>
-                            )
-                        })}
-                    </tbody>
-
-                </table>
+                {Object.keys(data).length >= 1 ? table : ''}
             </div>
         </div>
     )
